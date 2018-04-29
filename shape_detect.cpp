4124c7e9683ae98d786fee6582e559a92108a049
Harsimransingh <harsimran2421.harsimran2421@gmail.com>
2018-04-29 05:09:24 +0000

    working code with matrix

--- shape_detect.cpp
+++ shape_detect.cpp
@@ -24,8 +24,20 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h> 
+#include <sys/time.h> 
+#include <stdlib.h> 
+#include <stdio.h>
+#include <signal.h>
+#include <stdint.h>
 
-#include<signal.h>
+/*header file for wiring pi*/
+#include <wiringPi.h>
+
+#define DATA        0 // GPIO 17 (WiringPi pin num 0)  header pin 11
+#define CLOCK       3 // GPIO 22 (WiringPi pin num 3)   header pin 15
+#define LOAD        4 // GPIO 23 (WiringPi pin num 4)   header pin 16
+
 
 using namespace std;
 using namespace cv;
@@ -45,7 +57,7 @@ typedef enum __detect_shape_e {
  */
 
 /*initialising the semaphores*/
-sem_t service_shape_detect_sig, service_camera_cap_sig, service_shape_verify_sig, service_audio_sig;
+sem_t service_shape_detect_sig, service_camera_cap_sig, service_shape_verify_sig, service_audio_sig, service_led_mat_sig;
 
 /*gloabal variables for image capturing and processing*/
 cv::Mat imgOriginal;
@@ -53,10 +65,27 @@ raspicam::RaspiCam_Cv Camera;
 cv::Mat src;
 cv::Mat bw;
 
+/*2d array for led matrix*/
+unsigned char disp1[10][8]={
+  {0x3C,0x42,0x42,0x42,0x42,0x42,0x42,0x3C},//0
+  {0x10,0x30,0x50,0x10,0x10,0x10,0x10,0x10},//1
+  {0x7E,0x2,0x2,0x7E,0x40,0x40,0x40,0x7E},//2
+  {0x3E,0x2,0x2,0x3E,0x2,0x2,0x3E,0x0},//3
+  {0x8,0x18,0x28,0x48,0xFE,0x8,0x8,0x8},//4
+  {0x3C,0x20,0x20,0x3C,0x4,0x4,0x3C,0x0},//5
+  {0x3C,0x20,0x20,0x3C,0x24,0x24,0x3C,0x0},//6
+  {0x3E,0x22,0x4,0x8,0x8,0x8,0x8,0x8},//7
+  {0x0,0x3E,0x22,0x22,0x3E,0x22,0x22,0x3E},//8
+  {0x3E,0x22,0x22,0x3E,0x2,0x2,0x2,0x3E},//9
+};
+/*global variable to pass the number of sides to led_mat thread*/
+int sides = 0;
+
+
 char *error_string = "Wrong Shape, Try Again!";
 
 /* Global var that holds the shape detected */
-detect_shape_e shape = DEFAULT_SHAPE;
+detect_shape_e shape = SHAPE_CIRCLE;
 uint8_t shape_order = 0;
 
 static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
@@ -87,8 +116,73 @@ void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& cont
   cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
 }
 
+/*functions for led matrix code*/
+static void Send16bits (unsigned short output)
+{
+
+  unsigned char i;
+
+  for (i=16; i>0; i--) 
+  {
+    unsigned short mask = 1 << (i - 1); // calculate bitmask
+
+    digitalWrite(CLOCK, 0);  // set clock to 0
+
+    // Send one bit on the data pin
+
+    if (output & mask)   
+      digitalWrite(DATA, 1);          
+    else                              
+      digitalWrite(DATA, 0);  
+
+    digitalWrite(CLOCK, 1);  // set clock to 1
+
+  }
+
+}
+
+
+// Take a reg numer and data and send to the max7219
+
+static void MAX7219Send (unsigned char reg_number, unsigned char dataout)
+{
+  digitalWrite(LOAD, 1);  // set LOAD 1 to start
+  Send16bits((reg_number << 8) + dataout);   // send 16 bits ( reg number + dataout )
+  digitalWrite(LOAD, 0);  // LOAD 0 to latch
+  digitalWrite(LOAD, 1);  // set LOAD 1 to finish
+}
+
+
+void setup_led()
+{
+  MAX7219Send(0x09, 0x00);
+  MAX7219Send(0x0a, 0x10);
+  MAX7219Send(0x0b, 0x07);
+  MAX7219Send(0x0c, 0x01);
+  MAX7219Send(0x0f, 0x00);
+}
+
+void printnumber(int i)
+{
+  //cout << "In print Number\n" <<endl; 
+  for(int j = 1; j<9 ;j++)
+  {
+    MAX7219Send(j, disp1[i][j-1]);
+  }
+  delay(50);
+}
+
+
+
 void *camera_capture(void *args)
 {
+
+  struct timeval start;
+  struct timeval stop;
+  unsigned long int time_executed_us = 0;
+  double current_time;
+  
+  
   Camera.set(CV_CAP_PROP_FRAME_WIDTH,320);
   Camera.set(CV_CAP_PROP_FRAME_HEIGHT,240);
   Camera.set(CV_CAP_PROP_BRIGHTNESS,50);
@@ -104,14 +198,17 @@ void *camera_capture(void *args)
   {
     cout << "Cannot open the web cam" << endl;
   }
-  
+  int n = 0; 
   while(1) { 
     sem_wait(&service_camera_cap_sig);
+  
+    gettimeofday(&start, (struct timezone *)0);
 
     bool bSuccess = Camera.grab(); // read a new frame from video
 
     if (!bSuccess) //if not success, break loop
     {
+  if (wiringPiSetup () == -1) exit (1) ;
       cout << "Cannot read a frame from video stream" << endl;
       //break;
     }
@@ -138,17 +235,29 @@ void *camera_capture(void *args)
 
     // Use Canny instead of threshold to catch squares with gradient shading
     cv::Canny(gray, bw, 0, 50, 5);
-    cv::waitKey(333);
+    cv::waitKey(3); 
+    gettimeofday(&stop, (struct timezone *)0);
     
+    time_executed_us =((stop.tv_sec - start.tv_sec)*1000000) + ((stop.tv_usec - start.tv_usec));
+    printf("Service_1 time: %d\n", time_executed_us);
+    n++;
     sem_post(&service_shape_detect_sig);
+    
 
   }
 }
 
 void *shape_detection(void *args)
 {
+  struct timeval start;
+  struct timeval stop;
+  unsigned long int time_executed_us = 0;
+  double current_time;
+
+
   while(1) {
     sem_wait(&service_shape_detect_sig);
+    gettimeofday(&start, (struct timezone *)0);
 
     // Find contours
     std::vector<std::vector<cv::Point> > contours;
@@ -173,6 +282,7 @@ void *shape_detection(void *args)
       } else if (approx.size() == 3) { //detects annything with three sides ==>needs to be looked at 
         shape = SHAPE_TRI;
         setLabel(dst, "TRI", contours[i]);    // Triangles
+        //sides = 3;
       } else if (approx.size() >= 4 && approx.size() <= 6) {
 
         // Number of vertices of polygonal curve
@@ -196,12 +306,15 @@ void *shape_detection(void *args)
         if (vtc == 4 && mincos >= -0.1 && maxcos <= 0.3) {
           shape = SHAPE_RECT;
           setLabel(dst, "RECT", contours[i]);
+          //sides = 4;
         } else if (vtc == 5 && mincos >= -0.34 && maxcos <= -0.27) {
           shape = SHAPE_PENTA;
           setLabel(dst, "PENTA", contours[i]);
+          //sides = 5;
         } else if (vtc == 6 && mincos >= -0.55 && maxcos <= -0.45) {
           shape = SHAPE_HEXA;
           setLabel(dst, "HEXA", contours[i]);
+          //sides = 6;
         }
       } else {
 
@@ -214,6 +327,7 @@ void *shape_detection(void *args)
             std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2) {
           shape = SHAPE_CIRCLE;  
           setLabel(dst, "CIR", contours[i]);
+          //sides = 0;
         }
       }
     }
@@ -225,6 +339,11 @@ void *shape_detection(void *args)
 
     /* Increment the shapes detected by one only! */ 
     shape_order++;
+    gettimeofday(&stop, (struct timezone *)0);
+    
+    time_executed_us =((stop.tv_sec - start.tv_sec)*1000000) + ((stop.tv_usec - start.tv_usec));
+    printf("Service_2 time: %d\n", time_executed_us);
+ 
     sem_post(&service_shape_verify_sig);
   }
 }
@@ -233,12 +352,21 @@ bool verified = false;
 void *shape_verify(void *params)
 {
   static uint8_t int_order = 10;
+  struct timeval start;
+  struct timeval stop;
+  unsigned long int time_executed_us = 0;
+  double current_time;
+
+
   while(1) 
   {
     sem_wait(&service_shape_verify_sig);
     //printf("In threads: %s\n", __func__);
+    gettimeofday(&start, (struct timezone *)0);
+
+#ifdef EXIT   
     if(int_order == shape) {
-      printf("int_order: %d; shape: %d\n", int_order, shape);
+      //printf("int_order: %d; shape: %d\n", int_order, shape);
       verified = true;
       int_order+= 10;
       //exit(0);
@@ -247,19 +375,78 @@ void *shape_verify(void *params)
       /* 1. Do an audio output indicating an error
        * 2. Call the camera cap functions again! 
        */
-      printf("int_order: %d; shape: %d\n", int_order, shape);
+    //  printf("int_order: %d; shape: %d\n", int_order, shape);
       verified = false;
     }
-    
-      sem_post(&service_audio_sig);
+#endif
+
+    if ( shape == SHAPE_RECT || shape == SHAPE_TRI || shape == SHAPE_CIRCLE || shape == SHAPE_PENTA || shape == SHAPE_HEXA )
+    {
+      verified = true;
+
+      if(shape==SHAPE_RECT && verified == true)
+        sides = 4;
+      else if(shape==SHAPE_CIRCLE && verified == true)
+        sides = 0; 
+      else if(shape==SHAPE_TRI && verified == true)
+        sides = 3; 
+
+      else if(shape==SHAPE_PENTA && verified == true)
+        sides = 5; 
+      else if(shape==SHAPE_HEXA && verified == true)
+        sides = 6; 
+      sem_post(&service_led_mat_sig);
+       delay(100);
+    }
+    else
+    {
+       verified = false;
+      sem_post(&service_led_mat_sig);
+       delay(100);
+    }
+
+
+
+    gettimeofday(&stop, (struct timezone *)0);
+    time_executed_us =((stop.tv_sec - start.tv_sec)*1000000) + ((stop.tv_usec - start.tv_usec));
+    printf("Service_3 time: %d\n", time_executed_us);
+   
       //festival_say_text("In function shape verify, valid condition");
   }
     return NULL;
 }
 
+void *led_matrix(void *params)
+{
+  int side1;
+    //if (wiringPiSetup () == -1) exit (1) ;
+    //setup_led();
+    while(1)
+    {
+      sem_wait(&service_led_mat_sig);
+      cout << sides << endl; 
+      // if(side1 != sides)
+      // {
+      //sides = 5;
+      delay(600);
+      printnumber(sides);
+      delay(600);
+      // }
+      // side1 = sides; 
+      sem_post(&service_audio_sig);
+    }
+  return NULL; 
+
+}
 
 void *audio_output(void *params)
-{   
+{ 
+    struct timeval start;
+    struct timeval stop;
+    unsigned long int time_executed_us = 0;
+    double current_time;
+
+  
     int heap_size = 210000; 
     int load_init_files = 3; 
     festival_initialize(2, heap_size); 
@@ -267,19 +454,31 @@ void *audio_output(void *params)
   while(1)
   {
     sem_wait(&service_audio_sig);
-    
+    gettimeofday(&start, (struct timezone *)0);
+
     //if(shape==SHAPE_RECT)
-    if(shape==SHAPE_TRI && verified == true)
-      festival_say_file("./audiopart1.txt"); 
+    if(shape==SHAPE_RECT && verified == true)
+      //festival_say_file("./audiopart1.txt"); 
+      festival_say_text("rectangle");
     else if(shape==SHAPE_TRI && verified == true)
-      festival_say_file("./audiopart2.txt"); 
+      //festival_say_file("./audiopart2.txt"); 
+      festival_say_text("triangle");
     else if(shape==SHAPE_CIRCLE && verified == true)
-      festival_say_file("./audiopart3.txt"); 
+      //festival_say_file("./audiopart3.txt"); 
+      festival_say_text("circle");
     else if(shape==SHAPE_PENTA && verified == true)
-      festival_say_file("./audiopart4.txt"); 
+      festival_say_text("pentagon");
+    else if(shape==SHAPE_HEXA && verified == true)
+      festival_say_text("hexagon");
+    //festival_say_file("./audiopart4.txt"); 
     else if(verified == false)
-      festival_say_text(error_string);
-  
+      festival_say_text("error");
+
+    gettimeofday(&stop, (struct timezone *)0);
+
+    time_executed_us =((stop.tv_sec - start.tv_sec)*1000000) + ((stop.tv_usec - start.tv_usec));
+    printf("Service_4 time: %d\n", time_executed_us);
+    delay(100);
     sem_post(&service_camera_cap_sig);
   }
   return NULL;
@@ -289,20 +488,48 @@ void intHandler(int dummy) {
   cout<<"Signal Handler Caught!"<<endl;  
   exit(0);
 }
+#if 0
+int main()
+{
+	pthread_t service_led_mat;
+  
+  sem_init(&service_led_mat_sig,0,0);
 
+  if (wiringPiSetup () == -1) exit (1) ;
+  pinMode(DATA, OUTPUT);  
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LOAD, OUTPUT);  
 
+  setup_led();
+
+  pthread_create(&service_led_mat, NULL, led_matrix, NULL);
+  
+  sem_post(&service_led_mat_sig);
+  
+  pthread_join(service_led_mat, NULL);
+  
+  return 0;
+}
+#endif
+#if 1
 int main(int argc, char **argv)
 {
 
   signal(SIGINT, intHandler);
-
+  
   /* Create the services and the semaphores related to them */
-	pthread_t service_camera_cap, service_shape_detect, service_shape_verify, service_audio_output;
+	pthread_t service_camera_cap, service_shape_detect, service_shape_verify, service_audio_output, service_led_mat;
   sem_init(&service_camera_cap_sig,0,0);
   sem_init(&service_shape_detect_sig,0,0);
   sem_init(&service_shape_verify_sig,0,0);
   sem_init(&service_audio_sig,0,0);
-  /* Init. the TTS library */
+  sem_init(&service_led_mat_sig,0,0);
+  
+  if (wiringPiSetup () == -1) exit (1) ;
+  pinMode(DATA, OUTPUT);  
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LOAD, OUTPUT);  
+ /* Init. the TTS library */
   //int heap_size = 210000; 
   //int load_init_files = 1; 
   //festival_initialize(load_init_files, heap_size); 
@@ -310,11 +537,14 @@ int main(int argc, char **argv)
   //festival_say_text("hello world, creating threads...");
   //festival_say_file("./sampletextfile.txt"); 
 
+  setup_led();
+
   /* Create the threads */
 	pthread_create(&service_camera_cap, NULL, camera_capture, NULL);
 	pthread_create(&service_shape_detect, NULL, shape_detection, NULL);
 	pthread_create(&service_shape_verify, NULL, shape_verify, NULL);
 	pthread_create(&service_audio_output, NULL, audio_output, NULL);
+  pthread_create(&service_led_mat, NULL, led_matrix, NULL);
   sem_post(&service_camera_cap_sig);
 
   /* Wait for the threads to expire */
@@ -322,11 +552,14 @@ int main(int argc, char **argv)
   pthread_join(service_shape_detect,NULL);
   pthread_join(service_shape_verify,NULL);
   pthread_join(service_audio_output,NULL);
+  pthread_join(service_led_mat, NULL);
 	
   /* Destory the created semaphores */
   sem_destroy(&service_camera_cap_sig);
   sem_destroy(&service_shape_detect_sig);
   sem_destroy(&service_shape_verify_sig);
   sem_destroy(&service_audio_sig);
+  sem_destroy(&service_led_mat_sig);
   return 0;
 }
+#endif
